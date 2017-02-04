01bbb28b144f28eeb10acc43ac01fe5a924b6aaa
knyte <galactaknyte@gmail.com>
2017-02-04 18:25:29 -0500

    fixed coverage error

---
+++ resources/league.py
@@ -0,0 +1,21 @@
+#########################
+# league.py
+# handles a single league
+#########################
+
+# constants
+
+
+class League(object):
+
+    # main League class
+    ## takes a games Table,
+    ## a teams Table,
+    ## and a constants dictionary
+    def __init__(self, games, teams, constants):
+        self.games = games
+        self.teams = teams
+        self.constants = constants
+        self.team_size = self.constants.get("TEAM_SIZE", 1)
+        self.game_size = self.constants.get("GAME_SIZE", 2)
+        self.rating_system = self.constants.get("SYSTEM", "ELO")
