80eec4fc2d3f8a9cf3a3a0a8a280876afb234b8f
knyte <galactaknyte@gmail.com>
2017-03-31 12:24:01 -0500

    added references to fake credentials

---
+++ resources/constants.py
@@ -0,0 +1,13 @@
+# imports
+ from os.path import normpath
+ 
+ # functions
+ def getOwnDir():
+     baseDir = __file__
+     if baseDir[-3:] == "pyc":
+         return baseDir[:-(len("constants.pyc"))]
+     return baseDir[:-(len("constants.py"))]
+ 
+ BASE_DIR = normpath(getOwnDir() + "..") + "/"
+ API_CREDS = (BASE_DIR + "fake-credentials/wl_api.json")
+ GOOGLE_CREDS = (BASE_DIR + "fake-credentials/google_creds.json")
