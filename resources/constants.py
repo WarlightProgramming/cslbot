f3ea3c7928f2759ecebc4e6188ad6e4aec4e1642
knyte <galactaknyte@gmail.com>
2017-03-31 12:36:36 -0500

    fixed paths to fake credentials

--- resources/constants.py
+++ resources/constants.py
@@ -9,5 +9,5 @@
      return baseDir[:-(len("constants.py"))]
  
  BASE_DIR = normpath(getOwnDir() + "..") + "/"
- API_CREDS = (BASE_DIR + "fake-credentials/wl_api.json")
- GOOGLE_CREDS = (BASE_DIR + "fake-credentials/google_creds.json")
+ API_CREDS = (BASE_DIR + "resources/fake-credentials/wl_api.json")
+ GOOGLE_CREDS = (BASE_DIR + "resources/fake-credentials/google_creds.json")
