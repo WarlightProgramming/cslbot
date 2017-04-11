8657de263c03f8e600b7caa9ed69f719bd218a81
knyte <galactaknyte@gmail.com>
2017-04-11 13:18:39 -0500

    *sigh* secrets, secrets, secrets

--- resources/constants.py
+++ resources/constants.py
@@ -11,3 +11,4 @@ def getOwnDir():
 BASE_DIR = normpath(getOwnDir() + "..") + "/"
 API_CREDS = (BASE_DIR + "resources/fake-credentials/wl_api.json")
 GOOGLE_CREDS = (BASE_DIR + "resources/fake-credentials/google_creds.json")
+TIMEFORMAT = "%Y-%m-%d %H:%M:%S"
