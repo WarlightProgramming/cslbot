4cb0d4e27e54d8b3e28f63e1c7f4855fcc68b442
knyte <galactaknyte@gmail.com>
2017-04-12 13:55:52 -0500

    added debug keys

--- resources/constants.py
+++ resources/constants.py
@@ -12,3 +12,4 @@ BASE_DIR = normpath(getOwnDir() + "..") + "/"
 API_CREDS = (BASE_DIR + "resources/fake-credentials/wl_api.json")
 GOOGLE_CREDS = (BASE_DIR + "resources/fake-credentials/google_creds.json")
 TIMEFORMAT = "%Y-%m-%d %H:%M:%S"
+DEBUG_KEY = "DEBUG MODE"
