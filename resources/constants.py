75f7bd42be0925114859ed545ca2c3782215ad61
knyte <galactaknyte@gmail.com>
2017-04-13 21:50:15 -0500

    tracks iterations per day

--- resources/constants.py
+++ resources/constants.py
@@ -13,3 +13,4 @@ API_CREDS = (BASE_DIR + "resources/fake-credentials/wl_api.json")
 GOOGLE_CREDS = (BASE_DIR + "resources/fake-credentials/google_creds.json")
 TIMEFORMAT = "%Y-%m-%d %H:%M:%S"
 DEBUG_KEY = "DEBUG MODE"
+ITERATIONS_PER_DAY = 6
