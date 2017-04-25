21f7015973f60b1b2480331b47054c9f0f2164cd
knyte <galactaknyte@gmail.com>
2017-04-25 18:40:55 -0500

    finished global manager class

--- resources/global_manager.py
+++ resources/global_manager.py
@@ -8,17 +8,13 @@ import random
 import string
 from passlib.hash import pbkdf2_sha256 as token_hash
 
-# exceptions
-class AuthError(Exception):
-    """raised for failed authentication"""
-    pass
-
 # main GlobalManager class
 class GlobalManager(object):
     # constants
     ADMIN_SHEET = {'ID': 'UNIQUE INT',
                    'AUTHORIZED': 'STRING',
                    'BANNED': 'BOOL'}
+    SEP_ID = ";"
     AGENT_SHEET = {'ID': 'UNIQUE INT',
                    'TOKEN HASH': 'STRING',
                    'BANNED': 'BOOL'}
@@ -46,15 +42,35 @@ class GlobalManager(object):
             _ in xrange(length))
 
     def updateAgentToken(self, agentID):
-        TOKEN_LEN, agentID = 64, int(agentID)
+        TOKEN_LEN = 64
         token = self._randStr(TOKEN_LEN)
         tokenHash = token_hash.hash(token)
         self.agents.updateMatchingEntities({'ID': agentID},
-            {'TOKEN HASH': tokenHash})
+            {'TOKEN HASH': tokenHash}, True)
+        return token
 
     def verifyAgent(self, agentID, token):
         found = self.agents.findEntities({'ID': agentID})
-        if not len(found): return False
-        found = found[0]
-        if str(found['BANNED']).lower() == 'true': return False
-        return token_hash.verify(int(agentID), found['TOKEN HASH'])
+        if (len(found) and str(found[0]['BANNED']).lower() != 'true'):
+            return token_hash.verify(token, found[0]['TOKEN HASH'])
+        return False
+
+    def _authorizeExistingAdmin(self, data, clusterID):
+        clusters = [v for v in data['AUTHORIZED'].split(self.SEP_ID) if len(v)]
+        clusters.append(str(clusterID))
+        self.admins.updateMatchingEntities({'ID': data['ID']},
+            {'AUTHORIZED': (self.SEP_ID).join(c for c in clusters)})
+
+    def _createNewAdmin(self, adminID, clusterID):
+        self.admins.addEntity({'ID': adminID, 'AUTHORIZED': str(clusterID)})
+
+    def updateAdmin(self, adminID, clusterID):
+        found = self.admins.findEntities({'ID': adminID})
+        if len(found): self._authorizeExistingAdmin(found[0], clusterID)
+        else: self._createNewAdmin(adminID, clusterID)
+
+    def verifyAdmin(self, adminID, clusterID):
+        found = self.admins.findEntities({'ID': adminID, 'BANNED': {'values':
+            [True, 'TRUE'], 'type': 'negative'}})
+        return (len(found) and
+                str(clusterID) in found[0]['AUTHORIZED'].split(self.SEP_ID))
--- resources/test/global_manager_tests.py
+++ resources/test/global_manager_tests.py
@@ -3,9 +3,10 @@
 
 # imports
 from unittest import TestCase, main as run_tests
-from nose.tools import assert_equals, assert_raises, assert_false, assert_true
-from mock import patch, MagicMock
-from resources.global_manager import GlobalManager, AuthError
+from nose.tools import assert_equals, assert_false, assert_true
+from resources.global_manager import GlobalManager
+from mock import MagicMock, patch
+from passlib.hash import pbkdf2_sha256 as token_hash
 
 # tests
 ## GlobalManager class tests
@@ -21,6 +22,52 @@ class TestGlobalManager(TestCase):
                       self.database.fetchTable.return_value)
         assert_equals(self.manager.agents,
                       self.database.fetchTable.return_value)
+
+    def test_randStr(self):
+        assert_equals(len(self.manager._randStr(490)), 490)
+
+    @patch('resources.global_manager.token_hash.hash')
+    @patch('resources.global_manager.GlobalManager._randStr')
+    def test_updateAgentToken(self, rand, hashFn):
+        hashFn.return_value = "hash"
+        rand.return_value = "token"
+        assert_equals(self.manager.updateAgentToken(43904309), "token")
+        self.manager.agents.updateMatchingEntities.assert_called_with({'ID':
+            43904309}, {'TOKEN HASH': 'hash'}, True)
+
+    def test_verifyAgent(self):
+        self.manager.agents.findEntities.return_value = list()
+        assert_false(self.manager.verifyAgent(1234, "token"))
+        self.manager.agents.findEntities.return_value = [{'BANNED': True},]
+        assert_false(self.manager.verifyAgent(1234, "token"))
+        expHash = token_hash.hash('token')
+        self.manager.agents.findEntities.return_value = [{'BANNED': "",
+            'ID': 1234, 'TOKEN HASH': expHash},]
+        assert_true(self.manager.verifyAgent(1234, "token"))
+
+    def test_updateAdmin(self):
+        self.manager.admins.findEntities.return_value = list()
+        self.manager.updateAdmin(1234, 5678)
+        self.manager.admins.addEntity.assert_called_with({'ID': 1234,
+            'AUTHORIZED': '5678'})
+        self.manager.admins.findEntities.return_value = [{'ID': 1234,
+            'AUTHORIZED': ''},]
+        self.manager.updateAdmin(1234, 91011)
+        self.manager.admins.updateMatchingEntities.assert_called_with({'ID':
+            1234}, {'AUTHORIZED': '91011'})
+        self.manager.admins.findEntities.return_value = [{'ID': 1234,
+            'AUTHORIZED': '1;2;3;4'},]
+        self.manager.updateAdmin(1234, 91011)
+        self.manager.admins.updateMatchingEntities.assert_called_with({'ID':
+            1234}, {'AUTHORIZED': '1;2;3;4;91011'})
+
+    def test_verifyAdmin(self):
+        self.manager.admins.findEntities.return_value = list()
+        assert_false(self.manager.verifyAdmin(1234, 5678))
+        self.manager.admins.findEntities.return_value = [{'AUTHORIZED':
+            '12;23;48'},]
+        assert_false(self.manager.verifyAdmin(1234, 5678))
+        assert_true(self.manager.verifyAdmin(1234, 12))
 
 # run tests
 if __name__ == '__main__':
