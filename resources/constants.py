f6776ea3efdd73b410f427b85c9362cc72b20546
knyte <galactaknyte@gmail.com>
2017-04-27 15:59:54 -0500

    fixed OS path handling

--- resources/constants.py
+++ resources/constants.py
@@ -1,16 +1,13 @@
 # imports
-from os.path import normpath
+from os.path import realpath, dirname, pardir, normpath, join as pathjoin
 
 # functions
 def getOwnDir():
-    baseDir = __file__
-    if baseDir[-3:] == "pyc":
-         return baseDir[:-(len("constants.pyc"))]
-    return baseDir[:-(len("constants.py"))]
+    return dirname(realpath(__file__))
 
-BASE_DIR = normpath(getOwnDir() + "..") + "/"
-API_CREDS = (BASE_DIR + "resources/fake-credentials/wl_api.json")
-GOOGLE_CREDS = (BASE_DIR + "resources/fake-credentials/google_creds.json")
+BASE_DIR = normpath(pathjoin(getOwnDir(), pardir))
+API_CREDS = pathjoin(BASE_DIR, "resources", "fake-credentials", "wl_api.json")
+GOOGLE_CREDS = pathjoin(BASE_DIR, "resources", "fake-credentials", "google_creds.json")
 TIMEFORMAT = "%Y-%m-%d %H:%M:%S"
 DEBUG_KEY = "DEBUG MODE"
 GLOBAL_MANAGER = ""
